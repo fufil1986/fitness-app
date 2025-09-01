a47c9e3aa05a29b2b7d060cf17627e848d3a956c
Ahmad R. <ahmadramadhan.ar@gmail.com>
2025-09-01 20:43:23 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Min första app funkar 🎉</h1>
+      <p>Jag byggde den själv med Next.js!</p>
+    </main>
+  );
+}
